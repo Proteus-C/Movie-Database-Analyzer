974349f6eabfd6f9e865317208ea8bd2c20543e2
Proteus-C <proteustchang@gmail.com>
2025-08-12 13:24:49 -0500

    Working on aggregating data

--- Analyzer.py
+++ Analyzer.py
@@ -1,10 +1,30 @@
 import json
+import pandas as pd
+import numpy as np
 
+json_file_path = 'Database.json'
+
+
+# With Pandas
+with open(json_file_path, 'r') as f:
+    data = json.load(f)
+#df = pd.json_normalize(data)
+#print(df.head())
+
+df = pd.read_json(json_file_path)
+print(df.head())
+#rating = df.groupby('rating')
+#rating.mean()
+
+
+
+
+'''
 movieList = {
     "movies": [
         {
             "title":"The Godfather",
-            "rating":8.7,
+            "rating":9.2,
             "year":"1972"
         },
         {
@@ -14,8 +34,43 @@ movieList = {
         },
         {
             "title":"Interstellar",
-            "rating":8.6,
+            "rating":8.7,
             "year":"2014"        
+        },
+        {
+            "title":"The Dark Knight",
+            "rating":9.1,
+            "year":"2008"        
+        },
+        {
+            "title":"Transformers",
+            "rating":7.1,
+            "year":"2007"        
+        },
+        {
+            "title":"Kung Fu Panda",
+            "rating":7.8,
+            "year":"2008"        
+        },
+        {
+            "title":"Sin City",
+            "rating":8.0,
+            "year":"2005"        
+        },
+        {
+            "title":"CoCo",
+            "rating":8.4,
+            "year":"2017"        
+        },
+        {
+            "title":"Dragonball Evolution",
+            "rating":2.5,
+            "year":"2009"        
+        },
+        {
+            "title":"Green Lantern",
+            "rating":5.5,
+            "year":"2011"        
         }
     ]
 }
@@ -26,3 +81,4 @@ with open("Database.json", 'w') as fp:
 
 with open("Database.json") as f:
     print(f.read())
+'''
