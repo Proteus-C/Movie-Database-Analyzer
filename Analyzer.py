7b2571c2763a4f7fb301baaa33f8ca689a427453
Proteus-C <proteustchang@gmail.com>
2025-08-13 13:14:32 -0500

    Can add movies and calcualte ratings before/after 2008

--- Analyzer.py
+++ Analyzer.py
@@ -1,32 +1,56 @@
 import json
 import pandas as pd
 
-json_file_path = 'Database.json'
+def addMovie(fileData, name, rating, year):
+    movieName = name
+    movieRating = rating
+    movieRelease = year
+
+    movieList = {"title":movieName, "rating":movieRating, "year":movieRelease}
+
+    with open("Database.json", 'a') as fp:
+        json.dump(movieList, fp)
+        fp.write("\n")
 
 
-# With Pandas
-with open(json_file_path, 'r') as f:
-    data = json.load(f)
-df = pd.json_normalize(data['movies'])
-#print(df.head())
+exit = False
+while exit != True:
+    #Gets the file path    
+    json_file_path = 'Database.json'
+    # Puts data into database with Pandas
+    with open(json_file_path, 'r') as fp:
+        data = pd.read_json(fp, lines = True)
 
-df['year'] = df['year'].astype(int)
-after08 = df[df['year'] > 2008]
-before08 = df[df['year'] <= 2008]
+    choice = input("Do you want to compare movie [ratings] before/after 2008 or [add] a movie to the list or exit?\n")
+    if choice == "add":
+        addMovieName = input("Enter a movie name: ")
+        addMovieRating = input("Enter the movie's rating: ")
+        addMovieYear= input("Enter the movie's release year: ")
+        addMovie(data, addMovieName, addMovieRating, addMovieYear)
+        print("done!")
+    elif choice == "ratings":
+        df = data['year'].astype(int)
+        dfR = data['rating']
 
-rating1 = after08['rating'].mean()
-rating2 = before08['rating'].mean()
+        #Set the values from each year into the variables
+        count1, count2, after08, before08 = 0, 0, 0, 0
+        for i in range(0, len(df)):
+            if df[i] > 2008:
+                after08 += dfR[i]
+                count1 += 1
+            elif df[i] <= 2008:
+                before08 += dfR[i]
+                count2 += 1
 
-print(f"Average rating after 2008: {rating1}")
-print(f"Average rating before 2008: {rating2}")
+        #Calculates the rating
+        rating1 = after08 / count1
+        rating2 = before08 / count2
 
+        #Prints results
+        print(f"Average rating after 2008: {rating1}")
+        print(f"Average rating before 2008: {rating2}")
 
-'''
-#If I want to implement a way to add onto the movies list in the future
-
-with open("Database.json", 'a') as fp:
-    json.dump(movieList, fp, indent = 4)
-
-with open("Database.json") as f:
-    print(f.read())
-'''
+    elif choice == "exit":
+        exit = True
+    else:
+        print("Enter either: [ratings] or [add] or [exit]")
